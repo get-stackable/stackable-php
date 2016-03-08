b8184854c7b34756e2dd726fa82abbb16e2e3c64
Parminder Klair <perminder.klair@gmail.com>
2016-03-08 01:11:25 +0000

    item update and create

--- README.md
+++ README.md
@@ -58,9 +58,39 @@ $result = $stackable->getItem('ITEM-ID-HERE');
 print_r($result);
 ```
 
+To create new item
+
+- First initialize, using PRIVATE KEY, (MAKE SURE YOU KEEP THIS KEY SAFE! MOSTLY ON SERVER SIDE)
+
+```php
+$stackable = new Stackable('YOUR-STACK-PRIVATE-KEY-HERE');
+```
+
+```php
+var dataToPost = [
+    name => 'John Doe',
+    age => 29
+];
+
+$result = $stackable->createItem('CONTAINER-ID-HERE', dataToPost);
+print_r($result);
+```
+
+To update an item
+
+```php
+var dataToPost = [
+    name => 'John Doe',
+    age => 29
+];
+
+$result = $stackable->updateItem('ITEM-ID-HERE', dataToPost);
+print_r($result);
+```
+
 To play with this library: [https://beta.tehplayground.com/D85vaCf8xDxKeb8S](https://beta.tehplayground.com/D85vaCf8xDxKeb8S)
 
 ## Todo
 
 - Write tests.
-- POST webhooks to containers
+- Test create and update item methods
--- src/Stackable.php
+++ src/Stackable.php
@@ -62,6 +62,15 @@ class Stackable
         return $this->getData('items/' . $itemId);
     }
 
+    public function createItem($containerId, $data)
+    {
+        return $this->doPost('items/', ['containerId' => $containerId], $data);
+    }
+
+    public function updateItem($itemId, $data)
+    {
+        return $this->doPut('items/' . $itemId, $data);
+    }
 
     /**
      * Do curl to API to get requested data
@@ -72,17 +81,74 @@ class Stackable
     {
         $url = self::API_URL . '/' . self::API_VERSION . '/' . $path . '?token=' . $this->token;
 
-		//do curl
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-		$data = curl_exec($ch);
-		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		curl_close($ch);
+    		//do curl
+    		$ch = curl_init();
+    		curl_setopt($ch, CURLOPT_URL, $url);
+    		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+    		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    		$data = curl_exec($ch);
+    		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    		curl_close($ch);
 
-		return ($httpcode >= 200 && $httpcode < 300) ? json_decode($data) : false;
-	}
+    		return ($httpcode >= 200 && $httpcode < 300) ? json_decode($data) : false;
+	  }
+
+    /**
+     * Do curl to API to POST requested data
+     * @param $path
+     * @return array
+     */
+    private function doPost($path, $params, $data)
+    {
+        $url = self::API_URL . '/' . self::API_VERSION . '/' . $path . '?token=' . $this->token . '&' . http_build_query($params);
+
+        foreach($data as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+        rtrim($fields_string, '&');
+
+    		//do curl
+    		$ch = curl_init();
+    		curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, count($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+    		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+    		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    		$data = curl_exec($ch);
+    		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    		curl_close($ch);
+
+    		return ($httpcode >= 200 && $httpcode < 300) ? json_decode($data) : false;
+	  }
+
+    /**
+     * Do curl to API to PUT requested data
+     * @param $path
+     * @return array
+     */
+    private function doPut($path, $data)
+    {
+        $url = self::API_URL . '/' . self::API_VERSION . '/' . $path . '?token=' . $this->token;
+
+        foreach($data as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+        rtrim($fields_string, '&');
+
+    		//do curl
+    		$ch = curl_init();
+    		curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($ch, CURLOPT_POST, count($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+    		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+    		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    		$data = curl_exec($ch);
+    		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    		curl_close($ch);
+
+    		return ($httpcode >= 200 && $httpcode < 300) ? json_decode($data) : false;
+	  }
 }
